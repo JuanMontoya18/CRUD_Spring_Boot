310401dd6a414358cffed7d78915bcd798e25400
Juan Montoya <juanmontoya18>
2023-10-14 20:07:56 -0500

    se la interface se implementa el metodo a los servicios

---
+++ src/main/java/com/app/crud/demo/servicio/EstudianteServicio.java
@@ -0,0 +1,10 @@
+package com.app.crud.demo.servicio;
+
+import java.util.List;
+
+import com.app.crud.demo.entidad.Estudiante;
+
+public interface EstudianteServicio {
+
+    public List<Estudiante> listarTodosLosEstudiantes();
+}
